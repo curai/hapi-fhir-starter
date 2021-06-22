8e892ec33e49c31e71ef9f2d03ecf6ec0e1095eb
Kyle Chin <kyle@curai.com>
2021-06-22 12:37:36 -0700

    initial spring boot project

---
+++ src/main/java/com/example/fhirserver/FhirserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.fhirserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhirserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fhirserver/FhirserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fhirserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhirserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
